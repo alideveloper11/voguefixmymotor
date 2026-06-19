916d922091a275c1dd3135c066e0df8a15bf6281
Sufyan Ali <sufyanjatts199@gmail.com>
2026-06-19 19:45:49 +0500

    Add GTM code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import "./globals.css";
@@ -18,8 +19,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" style={{colorScheme:"light"}}>
-
+      <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NT53VTCB');`}
+        </Script>
+      </head>
       <body className="bg-white text-black">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NT53VTCB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <div className="flex flex-wrap w-full">
           <div className="w-full">
             <Header />
